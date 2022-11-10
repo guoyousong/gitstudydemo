2e4e548f781a2c19003196f6aaa52f8c01a82a43
blackmatter <1396504145@qq.com>
2022-11-10 13:57:11 +0800

    项目初始化

---
+++ src/main/java/com/example/gitstudydemo/GitstudydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitstudydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstudydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitstudydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitstudydemo/GitstudydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitstudydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
